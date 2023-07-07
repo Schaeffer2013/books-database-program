451633e8b923f2286e8d27ef5c34f9005a54313f
Schaeffer2013 <r-sschaeffer@live.com>
2023-07-07 11:22:29 -0400

    added database model

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from models import (Base, session, Book, engine)
 #import models
 #main menu- add, search, analysis, exit, view
 #add books to data base- functions
@@ -6,3 +7,8 @@
 #search books
 #data cleaning
 #loop runs program to exit program and stops
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+
--- models.py
+++ models.py
@@ -7,27 +7,29 @@
 
 
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine('sqlite:///users.db', echo=False)
+engine = create_engine('sqlite:///books.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'users'
+class Book(Base):
+    __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published_date = Column('Publsihed', Date)
+    price = Column('Price', Integer)
+    
 
     def __repr__(self):
-        return f'<User(name={self.name}, fullname={self.fullname}, nickname={self.nickname})>'
+        return f'Title: {self.title} Author: {self.author} Publsihed: {self.published_date} Price: {self.price}'
     
 
 if __name__ == '__main__':
