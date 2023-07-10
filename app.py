26d4d362d5dd8189bd4b248f34843b6fdeb7e242
Schaeffer2013 <r-sschaeffer@live.com>
2023-07-10 09:37:44 -0400

    edit

--- app.py
+++ app.py
@@ -21,13 +21,24 @@ def menu():
                   \rPlease choose one of the options above.
                   \rA number from 1-5.
                   \rPress enter to try again.''')
+            
+
+def submenu():
+    while True:
+        print('''
+              \r1) Edit
+              \r2) Delet
+              \r3) Return main menu''')
+        choice = input('What would you like to do? ')
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            input('''
+                  \rPlease choose one of the options above.
+                  \rA number from 1-3.
+                  \rPress enter to try again.''')
 #import models
-#main menu- add, search, analysis, exit, view
 #add books to data base- functions
-#edit books
-# delete books
-#search books
-#data cleaning
 #loop runs program to exit program and stops
 
 def clean_date(date_str):
@@ -41,10 +52,10 @@ def clean_date(date_str):
     except ValueError:
         input('''
               \n***** DATE ERROR *****
-              \rThe date format should include a valid Month Day, Year from the past.'
+              \rThe date format should include a valid Month Day, Year from the past.
               \rEx: January 13, 2003
               \rPress enter to try again.
-              \r*************''')
+              \r**********************''')
         return
     else:
         return return_date
@@ -59,7 +70,7 @@ def clean_price(price_str):
               \rThe price should be a number without a currency symbol.
               \rEx: 10.99
               \rPress enter to try again.
-              \r************''')
+              \r***********************''')
     else:
         return int(price_float * 100)
     
@@ -68,21 +79,44 @@ def clean_id(id_str, options):
         book_id = int(id_str)
     except ValueError:
         input('''
-              \n******ID ERROR ******
+              \n****** ID ERROR ******
               \rThe id should be a number.
               \rPress enter to try again.
-              \r************''')
+              \r**********************''')
         return
     else:
         if book_id in options:
             return book_id
         else:
-            input('''
-              \n******ID ERROR ******
+            input(f'''
+              \n****** ID ERROR ******
               \rOptions: {options}
               \rPress enter to try again.
-              \r************''')
+              \r**********************''')
             return
+        
+def edit_check(column_name, current_value):
+    print(f'\n**** EDIT {column_name}  ****')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name == 'Date':
+        print(f'\rCurrent Value: {current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}')
+
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to?')
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+    else:
+        return input('What would you like to change the value to? ')
 
 
     
@@ -114,7 +148,7 @@ def app():
             author = input('Author: ')
             date_error = True
             while date_error:
-                date = input('Published Date: (Ex: October 25, 2017): ')
+                date = input('Published Date (Ex: October 25, 2017): ')
                 date = clean_date(date)
                 if type(date) == datetime.date:
                     date_error = False
@@ -152,8 +186,25 @@ def app():
                   \n{the_book.title} by {the_book.author}
                   \rPublished: {the_book.published_date}
                   \rPrice: ${the_book.price / 100}''')
-            input('\nPress enter to return to the main menu.')
-
+            
+            
+            sub_choice = submenu()
+            if sub_choice == '1':
+                #edit
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book updated!')
+                time.sleep(1.5)
+                
+            elif sub_choice == '2':
+                #delete
+                 session.delete(the_book)
+                 session.commit()
+                 print('Book deleted!')
+                 time.sleep(1.5)
         elif choice == '4':
             # analysis
             pass
